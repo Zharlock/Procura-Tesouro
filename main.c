60228f0199ec7b8ea49e7c4e6add152fd9222cc8
Bagulho358 <127789150+Bagulho358@users.noreply.github.com>
2023-04-11 10:24:59 +0100

    Update main.c
    atualizei a variavel no main, para "dir", e defini quando chamamos a função posição a variavel direção vem como referencia "&dir", assim ele guarda a posição que digitamos.

--- main.c
+++ main.c
@@ -3,30 +3,30 @@
 #include <time.h>
 #include <math.h>
 #include "library.h"
-
 // Projeto da Disciplina Programação II (liguagem C), baseado num jogo de caça ao tesouro. Projeto elaborado por Carlos e João.
 
-int main() 
+int main()
     {
         int x = 0, y = 0;
         int linha_te, coluna_te, rowplayer, clplayer, count;
-        char direcao;
-    
+        int dir;
+   
         int nivel = obterCoordenadas(&x, &y);
         char **matriz = criarMatriz(x, y);
         tesourogen(&linha_te, &coluna_te, nivel);
         coordenadasinicio(&rowplayer, &clplayer);
-        updatetabuleiro(matriz, x, y, rowplayer, clplayer);
+        tabuleiro(matriz, x, y, rowplayer, clplayer, dir);
 
         //chama a função get_distance e calcula a distancia entre o tesouro e a posição do jogador
 
-        int distance = get_distance(rowplayer, clplayer, linha_te, coluna_te); 
+        int distance = get_distance(rowplayer, clplayer, linha_te, coluna_te);
 
         // motor do jogo
-        while (distance !=0) 
+
+        while (distance !=0)
             {
-                posicao(matriz, direcao, &rowplayer, &clplayer, linha_te, coluna_te, x , y);
-                updatetabuleiro(matriz, x, y, rowplayer, clplayer);
+                posicao(matriz, &dir, &rowplayer, &clplayer,linha_te,coluna_te, x , y);
+                updatetabuleiro(matriz, x, y, rowplayer, clplayer, dir);
                 distance = get_distance(rowplayer, clplayer, linha_te, coluna_te);
                 printf("Distância até o tesouro: %d\n", distance);
                  count = count+1;
@@ -34,6 +34,6 @@ int main()
 
         // O jogador encontrou o tesouro
         printf("Parabéns!! Chegou ao Tesouro em %d passos!! Ganho 100.000.000\n" , count);
-        
         return 0;
+
     }
